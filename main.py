4aa680313d0729b2f10d8c4c82d656e7e80406de
Duarte Nina <duartenina@tecnico.ulisboa.pt>
2026-03-06 15:00:38 +0100

    Changed svg library

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from src.create import convert_pgn_file_to_code
 from src.draw import draw_code
 
 
-FILENAME = "games/wcc_2023_game_18.pgn"
+FILENAME = "wcc_2023_game_18"
 # FILENAME = "games/sample_tiny.pgn"
 
 
 if __name__ == "__main__":
-    display(draw_code(convert_pgn_file_to_code(FILENAME)))
+    drawing = draw_code(convert_pgn_file_to_code(f"games/{FILENAME}.pgn"))
+    drawing.save_svg(f"images/{FILENAME}.svg")
--- src/draw.py
+++ src/draw.py
@@ -1,5 +1,4 @@
-# pyright: reportUnknownMemberType=false
-import svg
+import drawsvg as dw
 import numpy as np
 from .utils import index_to_coord
 
@@ -16,38 +15,37 @@ def coord_to_center_pos(
     return cx, cy
 
 
-def draw_base_board(n: int, square_size: float = 30) -> list[svg.Element]:
-    elements: list[svg.Element] = []
-
+def draw_base_board(svg: dw.Drawing, n: int, square_size: float = 30) -> dw.Drawing:
     for row in range(n):
         for col in range(n):
             x = row * square_size
             y = col * square_size
             color = SQUARE_COLORS[(row + col) % 2]
 
-            square = svg.Rect(
+            square = dw.Rectangle(
                 x=x, y=y, width=square_size, height=square_size, fill=color
             )
-            elements.append(square)
+            svg.append(square)
 
-    return elements
+    return svg
 
 
 def get_svg_for_piece(
     piece_in_fen: str, center: tuple[float, float] = (0, 0), piece_size: float = 25
-) -> svg.Element:
+) -> dw.Image:
     cx, cy = center
     cx, cy = cx - piece_size / 2, cy - piece_size / 2
 
     piece_u = piece_in_fen.upper()
     piece_color = "l" if piece_in_fen == piece_u else "d"
-    filename = f"svg/{piece_color}{piece_u}.svg"
+    filename = f"pieces/{piece_color}{piece_u}.svg"
 
-    return svg.Image(x=cx, y=cy, width=piece_size, height=piece_size, href=filename)
+    return dw.Image(
+        x=cx, y=cy, width=piece_size, height=piece_size, path=filename, embed=True
+    )
 
 
-def draw_pieces(code: str, square_size: float = 30) -> list[svg.Element]:
-    elements: list[svg.Element] = []
+def draw_pieces(svg: dw.Drawing, code: str, square_size: float = 30) -> dw.Drawing:
     piece_size = square_size * 0.9
 
     for ind, piece in enumerate(code):
@@ -57,27 +55,24 @@ def draw_pieces(code: str, square_size: float = 30) -> list[svg.Element]:
         coord = index_to_coord(ind)
         center = coord_to_center_pos(coord, square_size)
         piece_svg = get_svg_for_piece(piece, center, piece_size)
-        elements.append(piece_svg)
+        svg.append(piece_svg)
 
-    return elements
+    return svg
 
 
-def draw_svg(size: float, elements: list[svg.Element]):
-    canvas = svg.SVG(
-        width=size,
-        height=size,
-        elements=elements,
-    )
+def draw_svg(size: float) -> dw.Drawing:
+    canvas = dw.Drawing(size, size)
 
     return canvas
 
 
-def draw_code(code: str, square_size: float = 30) -> svg.SVG:
+def draw_code(code: str, square_size: float = 30) -> dw.Drawing:
     n = int(np.ceil(np.sqrt(len(code))))
     print(n)
     size = square_size * n
 
-    return draw_svg(
-        size,
-        draw_base_board(n, square_size=square_size) + draw_pieces(code, square_size),
-    )
+    svg = draw_svg(size)
+    svg = draw_base_board(svg, n, square_size)
+    svg = draw_pieces(svg, code, square_size)
+
+    return svg
